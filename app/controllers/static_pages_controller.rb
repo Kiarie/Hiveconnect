5c04864145788bdb300d73b118ddc47974716fa7
kiarie mburu <kiariemburus@gmail.com>
2013-11-20 19:38:31 +0300

    final commit before push

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,9 @@
 class StaticPagesController < ApplicationController
   def Home
+  if sign_in?
   @micropost = current_user.microposts.build if sign_in?
   @feed_items = current_user.feed.paginate(page: params[:page])
+  end
   end
 
   def Help
