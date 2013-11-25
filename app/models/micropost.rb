d4a3ab74a74c0998ad2d5c061f44aaf8a376ec67
kiarie mburu <kiariemburus@gmail.com>
2013-11-25 19:26:25 +0300

    finalised posts with following posts included

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -5,4 +5,9 @@ validates :content, presence: true, length: {maximum: 140}
 belongs_to :hive
 
 default_scope order: 'microposts.created_at DESC'
+ 
+def self.from_users_followed_by(hive)
+	followed_hive_ids = "SELECT followed_id FROM relationships WHERE follower_id = :user_id"
+	where("hive_id IN (#{followed_hive_ids}) or hive_id = :user_id", user_id: hive.id)
+	end
 end
