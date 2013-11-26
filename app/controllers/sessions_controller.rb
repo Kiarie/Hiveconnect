3154f3543179b9fac7b4af73260ac103b5dfd98b
kiarie mburu <kiariemburus@gmail.com>
2013-11-26 15:48:48 +0300

    commit with final brush up on errors

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,7 @@ def create
 	hive_user =  Hive.find_by_email(params[:session][:email])
 	if hive_user && hive_user.authenticate(params[:session][:password])
 	sign_in hive_user
-	redirect_back_or hive_user
+	redirect_back_or root_path
 	else
 	flash.now[:notice] = "invalid password or email"
 	render 'new'
